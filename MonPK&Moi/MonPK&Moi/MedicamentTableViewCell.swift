a054ee04a4bd55bbf6c7c4528b5364aaee798d52
Joris GRANEL <joris.granel@igtp4-mac05.isim.intra>
2018-02-28 11:19:16 +0100

    view

---
+++ MonPK&Moi/MonPK&Moi/MedicamentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MedicamentTableViewCell.swift
+//  MonPK&Moi
+//
+//  Created by Joris GRANEL on 28/02/2018.
+//  Copyright © 2018 Joris GRANEL. All rights reserved.
+//
+
+import UIKit
+
+class MedicamentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
