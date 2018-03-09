59d77df30e6bf3f2a414f4bd4bdb90dcad215a16
Justin ESCALIER <justin.escalier@igtp4-mac08.isim.intra>
2018-03-09 09:21:04 +0100

    update controller AjoutMedecin

--- MonPK&Moi/MonPK&Moi/AjoutContactMedicalViewController.swift
+++ MonPK&Moi/MonPK&Moi/AjoutContactMedicalViewController.swift
@@ -19,7 +19,36 @@ class AjoutContactMedicalViewController: UIViewController {
     @IBOutlet weak var textLieu: UITextField!
     
     @IBAction func ValidateButton(_ sender: Any) {
-        
+        self.saveNewMedecin(withName: textNom.text, withNum: textNum.text, withMail: textMail.text, withPrenom: textPrenom.text, withMetier: textMetier.text, withLieu: textLieu.text)
+    }
+    
+    func saveNewMedecin(withName nom: String?,withNum num: String?,withMail mail: String?,withPrenom prenom: String?, withMetier metier: String?, withLieu lieu: String?){
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else{
+            self.alertError(errorMsg: "Le contact ne peut être ajouté", userInfo: "raison inconnu")
+            return
+        }
+        let context = appDelegate.persistentContainer.viewContext
+        let medecin = Medecin(context: context)
+        medecin.nom=nom
+        medecin.telephone=num
+        medecin.email=mail
+        medecin.prenom=prenom
+        medecin.profession=metier
+        medecin.lieu_travail=lieu
+        do{
+            try context.save()
+        }
+        catch let error as NSError{
+            self.alertError(errorMsg: "\(error)", userInfo: "\(error.userInfo)")
+            return
+        }
+    }
+    
+    func alertError(errorMsg error: String, userInfo user: String=""){
+        let alert=UIAlertController(title: error, message: user, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(cancelAction)
+        present(alert,animated: true)
     }
     
     override func viewDidLoad() {
