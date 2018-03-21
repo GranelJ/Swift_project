ef07926948588a2087220dfcb40ba4568189c76e
Joris GRANEL <joris.granel@igtp4-mac05.isim.intra>
2018-03-21 18:16:01 +0100

    edit storyboard, manage addRDV

--- MonPK&Moi/MonPK&Moi/AjouterRendezVousViewController.swift
+++ MonPK&Moi/MonPK&Moi/AjouterRendezVousViewController.swift
@@ -8,10 +8,21 @@
 
 import UIKit
 
-class AjouterRendezVousViewController: UIViewController, UITextFieldDelegate {
+class AjouterRendezVousViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    var pickerData: [MedecinDAO] = []
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var libelleField: UITextField!
+    @IBOutlet weak var medecinPiker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        do{
+            try pickerData = MedecinDAO.getAll()
+        }catch let error as NSError{
+            ManageErrorHelper.alertError(view: self, WithTitle: "\(error)", andMessage: "\(error.userInfo)")
+        }
         // Do any additional setup after loading the view.
     }
 
@@ -25,16 +36,39 @@ class AjouterRendezVousViewController: UIViewController, UITextFieldDelegate {
         return true
     }
     
-    //TODO
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func ValidateButton(_ sender: Any) {
+        let medecinrow = medecinPiker.selectedRow(inComponent: 0)
+        let medecin = pickerData[medecinrow]
+        
+        self.saveNewRdv(withDate: datePicker.date, withLibelle: libelleField.text, withMedecin: medecin)
+        }
+    
+    func saveNewRdv(withDate date: Date?,withLibelle libelle: String?,withMedecin medecin: MedecinDAO?){
+        let rdv = RdvDAO(context: ManageCoreData.context)
+        rdv.date_rdv=date! as NSDate
+        rdv.libelle=libelle
+        rdv.rdv_medecin=medecin
+        
+        do{
+            try ManageCoreData.context.save()
+        }
+        catch let error as NSError{
+            ManageErrorHelper.alertError(view: self, WithTitle: "\(error)", andMessage: "\(error.userInfo)")
+            return
+        }
     }
-    */
+    
+    // MARK - Manage Picker
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row].prenom! + " " + pickerData[row].nom!
+    }
 
 }
