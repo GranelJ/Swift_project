c8b40763a14d899f6e2afc1793d307cf486feffa
Joris GRANEL <joris.granel@igtp4-mac05.isim.intra>
2018-03-22 15:33:59 +0100

    add : questionnaire should work

--- MonPK&Moi/MonPK&Moi/QuestionnaireViewController.swift
+++ MonPK&Moi/MonPK&Moi/QuestionnaireViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class QuestionnaireViewController: UIViewController {
 
+    @IBOutlet weak var switchON: UISwitch!
+    @IBOutlet weak var switchDYS: UISwitch!
+    @IBOutlet weak var switchOFF: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +25,40 @@ class QuestionnaireViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func ActSwitchON(_ sender: Any) {
+        switchDYS.setOn(false, animated: true)
+        switchOFF.setOn(false, animated: true)
+    }
 
+    @IBAction func ActSwitchDYS(_ sender: Any) {
+        switchON.setOn(false, animated: true)
+        switchOFF.setOn(false, animated: true)
+    }
+    
+    @IBAction func ActSwitchOFF(_ sender: Any) {
+        switchON.setOn(false, animated: true)
+        switchDYS.setOn(false, animated: true)
+    }
+    
+    @IBAction func validateButton(_ sender: Any) {
+        let test = Test_motriciteDAO(context: ManageCoreData.context)
+        test.date_test=Date() as NSDate
+        if(switchON.isOn){
+            test.reponse="ON"
+        }else if(switchDYS.isOn){
+            test.reponse="Dyskinesies"
+        }else if(switchOFF.isOn){
+            test.reponse="OFF"
+        }
+        do{
+            try ManageCoreData.context.save()
+        }
+        catch let error as NSError{
+            ManageErrorHelper.alertError(view: self, WithTitle: "\(error)", andMessage: "\(error.userInfo)")
+            return
+        }
+        self.navigationController?.popViewController(animated: true)
+    }
     /*
     // MARK: - Navigation
 
