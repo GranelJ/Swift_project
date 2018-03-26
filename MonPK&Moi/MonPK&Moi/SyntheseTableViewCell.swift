5678d5f31bebdda602191c205f818aded6c0125a
Joris GRANEL <joris.granel@igtp4-mac05.isim.intra>
2018-03-26 14:40:38 +0200

    add syntheseCell, add syntheseView

---
+++ MonPK&Moi/MonPK&Moi/SyntheseTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SyntheseTableViewCell.swift
+//  MonPK&Moi
+//
+//  Created by Joris GRANEL on 26/03/2018.
+//  Copyright © 2018 Joris GRANEL. All rights reserved.
+//
+
+import UIKit
+
+class SyntheseTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
